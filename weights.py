4750d68513fd87b90f75b5d652910bad7bea8d1f
jschiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2024-01-17 10:49:24 -0500

    gwei casting issues

--- weights.py
+++ weights.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+from decimal import Decimal
 
 df = pd.read_csv("odg_airdrop_unweighted.csv", header=0)
 
@@ -6,7 +7,8 @@ total_weight = 923440
 
 for i, row in df.iterrows():
     old_val = row["weight"]
-    df.at[i, 'weight'] = (row["weight"] / total_weight) * 300000 * 10 ** 18
+    df.at[i, 'weight'] = (Decimal(row["weight"]) / Decimal(total_weight) * Decimal("300000") * Decimal("10") ** 18).quantize(Decimal("1"))
+    df.at[i, 'weight'] = int(df.at[i, 'weight'])
     if i % 1000 == 0:
         print(f"calculated {i}. {old_val} now = {df.at[i, 'weight']}")
 
