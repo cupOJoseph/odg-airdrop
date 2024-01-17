2c02e18852f35b1176491b94e8c7f2accb0d29ae
jschiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2024-01-17 11:12:35 -0500

    decimals

--- weights.py
+++ weights.py
@@ -7,9 +7,8 @@ total_weight = 923440
 
 for i, row in df.iterrows():
     old_val = row["weight"]
-    df.at[i, 'weight'] = (Decimal(row["weight"]) / Decimal(total_weight) * Decimal("300000") * Decimal("10") ** 18).quantize(Decimal("1"))
-    df.at[i, 'weight'] = int(df.at[i, 'weight'])
-    if i % 1000 == 0:
+    df.at[i, 'weight'] = Decimal(row["weight"]) / Decimal(total_weight) * Decimal("300000") * Decimal("10") ** 18
+    if i % 5000 == 0:
         print(f"calculated {i}. {old_val} now = {df.at[i, 'weight']}")
 
 df.to_csv('final_odg_airdrop.csv', index=False, header=False)
